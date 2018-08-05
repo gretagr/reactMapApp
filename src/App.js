0c79d38fe8fff29e1113dd827132167eafdae71f
Greta <greta.grigelionyte@gmail.com>
2018-08-06 01:14:23 +0300

    infowindow for markers works fine, but map is re-rendering everytime marker clicked

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map'
 import NavBar from './components/NavBar'
-import MenuIcon from './components/MenuIcon'
 
 
 class App extends Component {
   state = {
     markers: [],
+
   }
 
   componentDidMount(){
@@ -15,23 +15,42 @@ class App extends Component {
   }
 
   getLocations () {
-    fetch('https://api.foursquare.com/v2/venues/search?ll=54.6821052,25.2759799&locale=en&categoryId=4bf58dd8d48988d163941735&client_id=TP4GRX0W4NPXOJDYDG10N2RZD5G4QMKCRQ1L45LSBXDNJOP1&client_secret=PKLYPTWTK4YZXEG0JNN0A3KGDSKU3E0PQCWTN1U2GIHCZLZS&v=20180804')
+    fetch('https://api.foursquare.com/v2/venues/search?ll=54.6872,25.2797&locale=en&categoryId=4bf58dd8d48988d163941735&client_id=TP4GRX0W4NPXOJDYDG10N2RZD5G4QMKCRQ1L45LSBXDNJOP1&client_secret=PKLYPTWTK4YZXEG0JNN0A3KGDSKU3E0PQCWTN1U2GIHCZLZS&v=20180804&limit=10')
     .then( response => response.json())
     .then(data => data.response.venues.map(place => (
       {
         id: place.id,
         name: place.name,
         lat: place.location.lat,
-        lng: place.location.lng
+        lng: place.location.lng,
+        address: place.location.formattedAddress,
+        image: 'https://s2.15min.lt/images/photos/2015/10/22/original/15ig20151022ozoparkas9615_result-5628b1fce91a2.jpg'
       }
     ))).then(markers => {
       this.setState({ markers: markers })
-    })
-
-
-    .catch(err => console.log(err))
+    }).catch(err => console.log(err))
 
 }
+
+
+
+
+// onToggleOpen = (openItemId, lat, lng) => {
+//   this.setState ({
+//     openItemId: openItemId,
+//     isOpen: true,
+//     zoom: 15,
+//     center: { lat, lng }
+//   })
+// }
+//   onToggleClose = (openItemId) => {
+//     this.setState ({
+//       openItemId: openItemId,
+//       isOpen: false,
+//       zoom: 13,
+//       center: { lat: 54.6872, lng: 25.2797 }
+//     })
+// }
 
   render() {
     console.log(this.state.markers);
@@ -42,6 +61,14 @@ class App extends Component {
          />
         <Map
           markers={this.state.markers}
+          // onToggleOpen={this.onToggleOpen}
+          // onToggleClose={this.onToggleClose}
+          // isOpen={this.state.isOpen}
+          // openitem={this.state.openItem}
+          // isMarkerShown={this.state.isMarkerShown}
+          // openItemId={this.state.openItemId}
+          // zoom={this.state.zoom}
+          // center={this.state.center}
         />
       </React.Fragment>
     )
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,63 +4,81 @@ import { GoogleMap, withGoogleMap, withScriptjs, Marker, InfoWindow } from 'reac
 import { FaAnchor } from 'react-icons/fa'
 
 export default class Map extends Component {
+  state = {
+    isOpen: false,
+    openItem: {},
+    openItemId: -1,
+    isMarkerShown: true,
+    zoom: 13,
+    center: { lat: 54.6872, lng: 25.2797 }
+  }
+
+  onToggleOpen = (openItemId, lat, lng) => {
+    this.setState ({
+      openItemId: openItemId,
+      isOpen: true,
+      zoom: 15,
+      center: { lat, lng }
+    })
+  }
+    onToggleClose = (openItemId) => {
+      this.setState ({
+        openItemId: openItemId,
+        isOpen: false,
+        zoom: 13,
+        center: { lat: 54.6872, lng: 25.2797 }
+      })
+  }
+
 
   render() {
 
     const mapStyles = require('../data/mapStyle.json')
-    const markers = this.props.markers.map(place => {
-      const marker = {
-        position: {
-          lat: place.lat,
-          lng: place.lng
-        },
+
+    const MyMap = compose(
+      withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBXIBdJCM-1cHLO86U1hfKVvXZ2HmQhhyo&v=3.exp&libraries=geometry,drawing,places",
+        containerElement: <div style={{ height: `100%`, width: '100%' }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+        loadingElement: <div style={{ height: `100%` }} />,
+      }),
+      withScriptjs,
+      withGoogleMap
+    )((props) =>
+      <GoogleMap
+        center = {this.state.center}
+        defaultZoom = {this.state.zoom}
+        options = {{
+          styles: mapStyles,
+          mapTypeControl: false
+        }}>
+
+      {this.state.isMarkerShown && this.props.markers.map( venue => {
+        return (
+        <Marker defaultAnimation={2}
+          key={venue.id}
+          position={{lat: venue.lat, lng: venue.lng}}
+          onClick={() =>  {this.onToggleOpen(venue.id, venue.lat, venue.lng)}}>
+          {this.state.isOpen && this.state.openItemId === venue.id &&
+          <InfoWindow className="info" onCloseClick={() => this.onToggleClose(venue.id, venue.lat, venue.lng)}>
+            <div>
+              <h1>{venue.name}</h1>
+              <img className="venue-image" src={venue.image}/>
+              <p>{venue.address}</p>
+          </div>
+          </InfoWindow>}
+        </Marker>
+      )
       }
-      return (
-      <Marker key={place.id} {...marker}>
-        <InfoWindow>
-          <FaAnchor/>
-        </InfoWindow>
-      </Marker>
+    )
+  }
+      </GoogleMap>
     )
 
-})
 
-    const MyMap = compose(withStateHandlers(() => ({
-      isOpen: false,
-    }), {
-      onToggleOpen: ({ isOpen }) => () => ({
-      isOpen: !isOpen,
-    })
-    }),
-    withScriptjs,
-    withGoogleMap
-    )(props =>
-    <GoogleMap
-      defaultCenter = { { lat: 54.6872, lng: 25.2797 } }
-      defaultZoom = { 13 }
-      options = {{
-        styles: mapStyles,
-        mapTypeControl: false
-      }}>
-      <Marker  onClick={props.onToggleOpen}>
-        {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-          <FaAnchor />
-        </InfoWindow>}
-      </Marker>
-      {props.isMarkerShown && markers } />}
-    </GoogleMap>
-
-  )
     return (
       <section className={this.props.openNav ? 'map-container active' : 'map-container'}>
-        <MyMap
-          googleMapURL= "https://maps.googleapis.com/maps/api/js?key=AIzaSyBXIBdJCM-1cHLO86U1hfKVvXZ2HmQhhyo&v=3.exp&libraries=geometry,drawing,places"
-          containerElement= {<div style={{ height: `100%`, width: '100%' }} />}
-          mapElement= {<div style={{ height: `100%` }} />}
-          loadingElement= {<div style={{ height: `100%` }} />}
-          isMarkerShown
-        />
-
+        <MyMap isMarkerShown/>
       </section>
     )
   }
